9d65ba3aa208247a709226960d0242f7f65187cd
Steffen Bruchmann <hi@sbruchmann.me>
2017-03-11 09:06:51 +0100

    Transform and style news metadata

--- extension/content.js
+++ extension/content.js
@@ -6,8 +6,39 @@ const wrapChildren = ($node, className) => {
 };
 
 const improveNewslistItem = ($item) => {
+	const $meta = document.createElement("div");
+	$meta.classList.add("meta");
+
 	const $address = $item.querySelector("address");
 	$address.textContent = $address.textContent.slice(3);
+
+	const upvotes = $item.querySelector(".upvotes").textContent;
+	const downvotes = $item.querySelector(".downvotes").textContent;
+	const score = parseInt(upvotes, 10) - parseInt(downvotes, 10);
+	const $score = document.createElement("span");
+	const suffix = (score !== -1 && score !== 1) ? "s" : "";
+	$score.classList.add("score");
+	$score.textContent = `${score} point${suffix}`;
+	$score.title = `${upvotes} up, ${downvotes} down`;
+	$meta.appendChild($score);
+
+	const authorName = $item.querySelector("username").textContent;
+	const $author = document.createElement("a");
+	$author.classList.add("author");
+	$author.href = `/user/${authorName}`;
+	$author.textContent = authorName;
+	$meta.appendChild($author);
+
+	const timeAgo = $item.textContent.match(/\d+ (second|minute|hour|day)s? ago/)[0];
+	if (timeAgo) {
+		const $timeAgo = document.createElement("span");
+		$timeAgo.classList.add("timeAgo");
+		$timeAgo.textContent = timeAgo;
+		$meta.appendChild($timeAgo);
+	}
+
+	const $originalMeta = $item.querySelector("p");
+	$item.replaceChild($meta, $originalMeta);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
