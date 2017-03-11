a87908edd82429cf91f71502cf89981ad5353b7d
Steffen Bruchmann <hi@sbruchmann.me>
2017-03-11 08:29:17 +0100

    Remove prefix from newslist-items addresses

--- extension/content.js
+++ extension/content.js
@@ -1,10 +1,24 @@
 /*globals detectPage*/
 "use strict";
 
+const wrapChildren = ($node, className) => {
+	$node.innerHTML = `<div class="${className}">${$node.innerHTML}</div>`;
+};
+
+const improveNewslistItem = ($item) => {
+	const $address = $item.querySelector("address");
+	$address.textContent = $address.textContent.slice(3);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
 	const classList = document.documentElement.classList;
 
+	wrapChildren(document.querySelector(".container > header"), "rejs-wrapper");
+
 	if (detectPage.isNewsList()) {
 		classList.add("is-newslist");
+		document
+				.querySelectorAll("#newslist article")
+				.forEach(improveNewslistItem);
 	}
 });
